08c3f1985a867d22a30b836f783350dbf2ef614f
Phoebe Emefa <phoebeemefa@gmail.com>
2022-07-06 14:38:15 +0000

    added navbar and hero component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react";
+import NavBar from '../components/Home/NavBar';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Box fontFamily='body' overflowX='hidden'>
+        <NavBar />
+     <Box mt={20}>
+     <Component {...pageProps} />
+     </Box>
+      </Box>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
